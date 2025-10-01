e67c62488a68a29a238f837b66d371f95d9b7186
Steven Robertson <steverobertson@gmail.com>
2025-10-02 00:38:07 +0100

    Updating import to try fix issue in live build

--- apps/web/src/components/ProjectList.jsx
+++ apps/web/src/components/ProjectList.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {Link} from "react-router-dom";
-import formatDistance from "date-fns/formatDistance";
+import { formatDistance } from "date-fns";
 import {DataTable} from "primereact/datatable";
 import {Column} from "primereact/column";
 import {
